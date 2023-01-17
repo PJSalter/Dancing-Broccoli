57ac3afa026bb49385a2e971055191f70ab7b931
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-01-17 00:03:34 +0000

    functionality on button press for animation of dance to activate and stopped from event listener

---
+++ script.js
@@ -0,0 +1,18 @@
+const pressMeBroccoli = document.querySelector(".btn");
+const broccoli = document.querySelector(".broccoli");
+const textThatBroccoli = document.querySelector(".btn span");
+const shadowThoseGreens = document.querySelector(".shadow");
+
+function broccoliMilkDance() {
+   if (broccoli.classList.contains("anime")) {
+      broccoli.classList.remove("anime");
+      shadowThoseGreens.classList.remove("animeShadow");
+      textThatBroccoli.innerText = "Broccili milk anyone?";
+   } else {
+      broccoli.classList.add("anime");
+      shadowThoseGreens.classList.add("animeShadow");
+      textThatBroccoli.innerText = "can't milk this";
+   }
+};
+
+pressMeBroccoli.addEventListener("click", broccoliMilkDance);
