fcc7a16372c86820a994f95eda3cfd87128c02a2
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-01-17 00:07:29 +0000

    corrected error

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const textThatBroccoli = document.querySelector(".btn span");
 const shadowThoseGreens = document.querySelector(".shadow");
 
 function broccoliMilkDance() {
-   if (broccoli.classList.contains("anime")) {
-      broccoli.classList.remove("anime");
-      shadowThoseGreens.classList.remove("animeShadow");
-      textThatBroccoli.innerText = "Broccili milk anyone?";
-   } else {
-      broccoli.classList.add("anime");
-      shadowThoseGreens.classList.add("animeShadow");
-      textThatBroccoli.innerText = "can't milk this";
-   }
-};
+  if (broccoli.classList.contains("anime")) {
+    broccoli.classList.remove("anime");
+    shadowThoseGreens.classList.remove("animeShadow");
+    textThatBroccoli.innerText = "Broccoli milk anyone?";
+  } else {
+    broccoli.classList.add("anime");
+    shadowThoseGreens.classList.add("animeShadow");
+    textThatBroccoli.innerText = "can't milk this";
+  }
+}
 
 pressMeBroccoli.addEventListener("click", broccoliMilkDance);
